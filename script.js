710fe58508bb13cddbed7335574b8b49550dc4ff
Niscolinx <munisco12@gmail.com>
2020-02-01 14:27:41 +0100

    update of my missing files that where retrieved back from netlify

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-let footer = document.querySelector(".header__main--paragraph");
+//NAV BAR SHRINK ON SCROLL
+const navBackground = () =>{
+  let navBar = document.querySelector('#navbar');
+  window.onscroll = function() { 
+      if (document.documentElement.scrollTop) {
+        navBar.classList.add("nav-background");
+      } else {
+        navBar.classList.remove("nav-background");
+      }
+  }
+}  
+//SLIDING THE NAV BAR ON SMALLER SCREENS
+  const navSlide = () =>{
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-list');
+    const navEl = document.querySelectorAll('.nav-list__el');
 
-footer.addEventListener("click", function(){
-   this.textContent = '4rem'
-})
+    menu.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      menu.classList.toggle('toggle');
+      navEl.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        }
+        else{
+          link.style.animation = `navLinkSlide 1s ease forwards ${index / 7 + 0}s`
+        }
+      })
+    })
+  }
+
+  const app = () =>{
+    navBackground()
+    navSlide()
+  }
+
+  app();
